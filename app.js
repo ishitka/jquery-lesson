725b186a422ad3cc21fee9cf7cc301ccacd62aa7
EC2 Default User <ec2-user@ip-172-31-21-85.ap-northeast-1.compute.internal>
2024-06-11 13:22:40 +0000

    add

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 alert('Hello World');
 
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
-});
+
 
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
-  });
+  $('.box1').slideDown();
 });
