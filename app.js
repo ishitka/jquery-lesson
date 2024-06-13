f113c7432e89b5c79c508dffff01091aed7eccde
EC2 Default User <ec2-user@ip-172-31-21-85.ap-northeast-1.compute.internal>
2024-06-13 12:08:14 +0000

    add

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Hello World');
 
 
-
 $(function(){
-  $('.box1').slideDown();
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
